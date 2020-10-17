cea87d6ba9fbd8450564c70aab3c6ac40cf281b1
Sergeij Belevskij <bbelevskij@gmail.com>
2020-10-17 19:47:07 +0300

    iteration#16

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ app.get('/oauth.redirect', (request, response) => {
   const redirectUrl = [
     process.env.SLACK_OAUTH_REDIRECT_URL,
     `?client_id=${process.env.SLACK_CLIENT_ID}`,
-    '&scopes=chat:write:workspace'
+    '&scopes=chat:write:user'
   ].join('')
   response.redirect(redirectUrl)
 })
