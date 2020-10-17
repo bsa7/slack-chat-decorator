4a8154702705c85784a1707aeaf8a3b2f4b90652
Sergeij Belevskij <bbelevskij@gmail.com>
2020-10-17 13:19:22 +0300

    Initialized with express

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+const PORT = process.env.PORT || 3000
+const server = app.listen(PORT)
