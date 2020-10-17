d5ea4a320944fd5f3b8b6730aa75bc7696739523
Sergeij Belevskij <bbelevskij@gmail.com>
2020-10-17 21:29:57 +0300

    iteration#24

--- src/index.js
+++ src/index.js
@@ -54,10 +54,12 @@ const installer = new InstallProvider({
   stateSecret: 'my-state-secret',
   installationStore: {
     storeInstallation: (installation) => {
+      console.log('storeInstallation#57', { installation })
       myDB.save(installation.team.id, installation)
       return
     },
     fetchInstallation: (installQuery) => {
+      console.log('fetchInstallation#62', { installQuery })
       return myDB.getSync(installQuery.teamId)
     },
   },
@@ -65,19 +67,21 @@ const installer = new InstallProvider({
     // generateStateParam's first argument is the entire InstallUrlOptions object which was passed into generateInstallUrl method
     // the second argument is a date object
     // the method is expected to return a string representing the state
-    randomState: randomString(),
     generateStateParam: (installUrlOptions, date) => {
+      console.log('generateStateParam#69', { installUrlOptions, date })
       // generate a random string to use as state in the URL
       // save installOptions to cache/db
-      myDB.save(this.randomState, installUrlOptions)
+      const randomState = randomString()
+      myDB.save(randomState, installUrlOptions)
       // return a state string that references saved options in DB
-      return this.randomState
+      return randomState
     },
     // verifyStateParam's first argument is a date object and the second argument is a string representing the state
     // verifyStateParam is expected to return an object representing installUrlOptions
     verifyStateParam: (date, state) => {
+      console.log('#80', { date, state })
       // fetch saved installOptions from DB using state reference
-      const installUrlOptions = myDB.getSync(this.randomState)
+      const installUrlOptions = myDB.getSync(state)
       return installUrlOptions
     }
   }
