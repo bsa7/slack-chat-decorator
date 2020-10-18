15c2378fb663f80c0e80ad56bb8153b8de31af91
Sergeij Belevskij <bbelevskij@gmail.com>
2020-10-18 14:12:55 +0300

    deploy with pm2#2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,15 +3,15 @@ module.exports = {
     script: 'src/index.js',
     watch: '.'
   }],
-  deploy : {
-    production : {
-      user : 'slon',
-      host : '45.141.100.17',
-      ref  : 'origin/main',
-      repo : 'git@github.com:r72cccp/slack-chat-decorator.git',
-      path : '/home/slon/projects/slack-chat-decorator',
+  deploy: {
+    production: {
+      user: 'slon',
+      host: '45.141.100.17',
+      ref: 'origin/main',
+      repo: 'git@github.com:r72cccp/slack-chat-decorator.git',
+      path: '/home/slon/projects/slack-chat-decorator',
       'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
     }
   }
