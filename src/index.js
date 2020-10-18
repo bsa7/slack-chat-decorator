780e7d2385b002d64888cfe9270cb16330c1df85
Sergeij Belevskij <bbelevskij@gmail.com>
2020-10-18 16:54:29 +0300

    slack oauth installer #15

--- src/index.js
+++ src/index.js
@@ -26,13 +26,13 @@ app.post('/message.event', (request, response) => {
 app.get('/oauth.install', async (request, response, next) => {
   console.log('get/oauth.install#27')
   try {
-    // feel free to modify the scopes
     const url = await installer.generateInstallUrl({
-      scopes: ['channels:read', 'groups:read', 'channels:manage', 'chat:write', 'incoming-webhook'],
+      // scopes: ['channels:read', 'groups:read', 'channels:manage', 'chat:write', 'incoming-webhook'],
       // scopes: ['chat:write:user']
+      scopes: ['chat:write']
     })
 
-    response.send(`<a href=${url}><img alt=""Add to Slack"" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>`)
+    response.redirect(url)
   } catch(error) {
     console.log(error)
   }
